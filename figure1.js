3632a284b33e40a7ef4638711c7af8293d972aca
F1Mider <f1mider@hotmail.com>
2019-04-29 01:19:32 -0500

    test vegalite and reveal

--- figure1.js
+++ figure1.js
@@ -1,90 +1,87 @@
-var yourVlSpec = {
-    $schema: 'https://vega.github.io/schema/vega-lite/v3.0.json',
-    description: 'A simple bar chart with embedded data.',
-    data: {
-        name: 'champaign_zoning', url: './champaign.topojson',
-        "format": {
-            "type": "topojson",
-            "feature": "Zoning"
-        }
-    },
-    "transform": [
-        {
-            'lookup': 'properties.zoning_description',
-            'from': {
-                'fields': ['base_type'],
-                'data':
-                {
-                    'values': [
-                        { 'code': 'CB1', 'base_type': 'Commercial' },
-                        { 'code': 'CB2', 'base_type': 'Commercial' },
-                        { 'code': 'CB3', 'base_type': 'Commercial' },
-                        { 'code': 'CG', 'base_type': 'Commercial' },
-                        { 'code': 'CI', 'base_type': 'Commercial' },
-                        { 'code': 'CN', 'base_type': 'Commercial' },
-                        { 'code': 'CO', 'base_type': 'Commercial' },
-                        { 'code': 'I1', 'base_type': 'Industrial' },
-                        { 'code': 'I2', 'base_type': 'Industrial' },
-                        { 'code': 'IBP', 'base_type': 'Commercial' },
-                        { 'code': 'IOP', 'base_type': 'Commercial' },
-                        { 'code': 'IT-MF', 'base_type': 'Residential' },
-                        { 'code': 'IT-MX', 'base_type': 'Residential' },
-                        { 'code': 'IT-NC', 'base_type': 'Residential' },
-                        { 'code': 'IT-SF1', 'base_type': 'Residential' },
-                        { 'code': 'IT-SF2', 'base_type': 'Residential' },
-                        { 'code': 'MF1', 'base_type': 'Residential' },
-                        { 'code': 'MF2', 'base_type': 'Residential' },
-                        { 'code': 'MF3', 'base_type': 'Residential' },
-                        { 'code': 'MFUniv', 'base_type': 'Residential' },
-                        { 'code': 'MHP', 'base_type': 'Residential' },
-                        { 'code': 'SF1', 'base_type': 'Residential' },
-                        { 'code': 'SF2', 'base_type': 'Residential' }
-                    ]
-                },
-                'key': 'code'
-            }
-        }
-    ],
-    "hconcat": [
-        {
-            "selection": {
-                "zone_choice": {
-                    "fields": ["base_type"],
-                    "type": "single",
-                    "bind": { "input": "select", "options": ["Residential", "Industrial", "Commercial"] }
-                }
-            },
-            "mark": "geoshape",
-            "projection": {
-                "type": "albersUsa"
-            },
-            "encoding": {
-                "color": { "field": "base_type", "type": "nominal" }
-            },
-            "width": 600, "height": 400,
-            "autosize": {
-                "type": "fit",
-                "contains": "padding"
-            }
-        },
-        {
-            "mark": "bar",
-            "transform": [
-                { "filter": { "selection": "zone_choice" } }
-            ],
-            "encoding": {
-                "y": {
-                    "field": "properties.zoning_code",
-                    "type": "nominal"
-                },
-                "x": {
-                    "field": "properties.SHAPESTArea",
-                    "aggregate": "sum",
-                    "type": "quantitative"
-                }
-            }
-        }
-    ],
-};
+Reveal.addEventListener('ready', function() {
 
-var embedded = vegaEmbed('#vis', yourVlSpec);
+    var yourVlSpec = 
+    {
+        "$schema": "https://vega.github.io/schema/vega-lite/v3.0.json",
+        "description": "A simple bar chart with embedded data.",
+        "data": {
+          "url": "https://raw.githubusercontent.com/F1Mider/spr2019-adv-project/master/rides.csv"
+        },
+        "hconcat": [
+          {
+            "layer": [
+              {
+                "data": {
+                  "url": "https://raw.githubusercontent.com/F1Mider/spr2019-adv-project/master/N02-17_GML/N02-17_RailroadSection.geojson",
+                  "format": {"property": "features"}
+                },
+                "mark": "geoshape",
+                "encoding": {"color": {"value": "gray"}}
+              },
+              {
+                "selection": {
+                  "station_choice": {"fields": ["station_name_x"], "type": "single"},
+                  "type_choice": {
+                    "fields": ["type"],
+                    "type": "single",
+                    "bind": {
+                      "input": "select",
+                      "options": [null, "ＪＲ", "モノレール", "地下鉄", "新交通", "路面電車", "私鉄"]
+                    }
+                  }
+                },
+                "mark": "circle",
+                "encoding": {
+                  "longitude": {"field": "lon_x", "type": "quantitative"},
+                  "latitude": {"field": "lat_x", "type": "quantitative"},
+                  "tooltip": [
+                    {"field": "station_name_x", "type": "nominal"}
+                  ]
+                },
+                "width": 900,
+                "height": 1000
+              }
+            ]
+          },
+          {
+            "vconcat": [
+              {
+                "mark": "bar",
+                "transform": [{"filter": {"selection": "station_choice"}}],
+                "encoding": {
+                  "x": {"bin": true, "field": "distance", "type": "quantitative"},
+                  "y": {"aggregate": "count", "type": "quantitative"}
+                }
+              },
+              {
+                "mark": "bar",
+                "transform": [{"filter": {"selection": "station_choice"}}],
+                "encoding": {
+                  "y": {"field": "type", "type": "nominal"},
+                  "x": {"aggregate": "count", "type": "quantitative"}
+                }
+              },
+              {
+                "mark": "bar",
+                "transform": [{"filter": {"selection": "station_choice"}}],
+                "encoding": {
+                  "y": {"field": "seat", "type": "nominal"},
+                  "x": {"aggregate": "count", "type": "quantitative"}
+                }
+              },
+              {
+                "mark": "bar",
+                "transform": [{"filter": {"selection": "station_choice"}}],
+                "encoding": {
+                  "y": {"field": "service", "type": "nominal"},
+                  "x": {"aggregate": "count", "type": "quantitative"}
+                }
+              }
+            ]
+          }
+        ]
+      };
+    
+      var embedded = vegaEmbed('#vis', yourVlSpec);
+
+          });
